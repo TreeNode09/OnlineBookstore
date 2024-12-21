04ef07600a31bdc291bca02b107d175e24eedd00
HUSTERandENDER <15272984770@163.com>
2024-12-21 20:58:53 +0800

    OutOfStock

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import axios  from 'axios'
 
 import App from './App.vue'
 import router from './router'
+
+// axios.defaults.baseURL = 'http://localhost:8001'
 
 const app = createApp(App)
 app.use(createPinia())
--- src/views/OutOfStock.vue
+++ src/views/OutOfStock.vue
@@ -4,13 +4,13 @@
     <el-menu-item index="/purchaseList">采购单管理</el-menu-item>
 </el-menu>
 <h1>缺书</h1>
-<!-- <el-table :data="stocks">
+<el-table :data="stocks">
     <el-table-column v-for="key in Object.keys(stocks[0])" :prop="key" :label="key"></el-table-column>
-</el-table> -->
+</el-table>
 </template>
     
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref, onMount } from 'vue'
 import axios from 'axios'
 import { usePage } from '@/stores/page'
     
@@ -18,11 +18,15 @@ const page = usePage()
     
 const stocks = ref([])
     
-onMounted(() => {
+onMount(() => {
     page.currentPage = '/outOfStock'
     page.currentSubPage = '/outOfStock'
-    axios.get('', 
-    ).then(responce => {stocks.value = responce.data.data})
+    axios.get('http://localhost:8001/business/stockList'
+    ).then(response => {
+        console.log(response)
+        stocks.value = response.data.data
+        console.log(stocks.value)
+    })
     .catch(error => {alert(error)})
 })
 </script>
