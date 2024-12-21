aefc0f4460c7558d6e79d23d1cd5041de467260d
TreeNode09 <StickpageX@hotmail.com>
2024-12-21 21:01:16 +0800

    修改表格渲染时机

--- src/views/OutOfStock.vue
+++ src/views/OutOfStock.vue
@@ -4,28 +4,28 @@
     <el-menu-item index="/purchaseList">采购单管理</el-menu-item>
 </el-menu>
 <h1>缺书</h1>
-<el-table :data="stocks">
+<el-table v-if="isMounted" :data="stocks">
     <el-table-column v-for="key in Object.keys(stocks[0])" :prop="key" :label="key"></el-table-column>
 </el-table>
 </template>
     
 <script setup>
-import { ref, onMount } from 'vue'
+import { ref, onMounted } from 'vue'
 import axios from 'axios'
 import { usePage } from '@/stores/page'
     
 const page = usePage()
-    
+
+const isMounted = ref(false)
 const stocks = ref([])
     
-onMount(() => {
+onMounted(() => {
     page.currentPage = '/outOfStock'
     page.currentSubPage = '/outOfStock'
     axios.get('http://localhost:8001/business/stockList'
     ).then(response => {
-        console.log(response)
         stocks.value = response.data.data
-        console.log(stocks.value)
+        isMounted.value = true
     })
     .catch(error => {alert(error)})
 })
