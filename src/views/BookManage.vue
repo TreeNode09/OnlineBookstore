952520e74b093c56b278f5ac505fa6e86ab8f154
HUSTERandENDER <15272984770@163.com>
2024-12-23 21:06:58 +0800

    渡劫完成

--- src/views/BookManage.vue
+++ src/views/BookManage.vue
@@ -17,7 +17,7 @@
     <el-table-column prop="keyword" label="关键词" width="200px"></el-table-column>
     <el-table-column width="75px" fixed="right" >
         <template #default="scope">
-            <el-button :disabled="!isDelete" @click="deleteBook(scope.row)"
+            <el-button :disabled="!isDelete" @click="deleteBook(scope.row.bookId)"
                 type="danger" size="small" >删除</el-button>
         </template>
     </el-table-column>
@@ -39,18 +39,18 @@ const isMounted = ref(false)
 const isDelete = ref(false)
 
 const books = ref([
-    {bookId: 4, isbn: "978-7-108-04348-1", name: "活着", publish: "作家出版社", price: 29.0,
-    inventory: 40, author: "余华", keyword: "小说 人生"},
-    {bookId: 5, isbn: "978-7-5063-4079-4", name: "百年孤独", publish: "南海出版公司", price: 39.5,
-    inventory: 50, author: "加西亚·马尔克斯", keyword: "魔幻现实主义 文学"},
-    {bookId: 6, isbn: "978-7-5321-0295-8", name: "围城", publish: "人民文学出版社", price: 32.0,
-    inventory: 60, author: "钱钟书", keyword: "讽刺 小说"},
-    {bookId: 7, isbn: "978-7-02-010804-5", name: "三体", publish: "重庆出版社", price: 68.0,
-    inventory: 80, author: "刘慈欣", keyword: "科幻 小说"},
-    {bookId: 8, isbn: "978-7-108-04348-1", name: "平凡的世界", publish: "人民文学出版社", price: 39.8,
-    inventory: 70, author: "路遥", keyword: "现实主义 小说"},
-    {bookId: 9, isbn: "978-7-123-45678", name: "新书", publish: "新出版社", price: 45.0,
-    inventory: 252, author: "新作者", keyword: "新书 出版"}
+    // {bookId: 4, isbn: "978-7-108-04348-1", name: "活着", publish: "作家出版社", price: 29.0,
+    // inventory: 40, author: "余华", keyword: "小说 人生"},
+    // {bookId: 5, isbn: "978-7-5063-4079-4", name: "百年孤独", publish: "南海出版公司", price: 39.5,
+    // inventory: 50, author: "加西亚·马尔克斯", keyword: "魔幻现实主义 文学"},
+    // {bookId: 6, isbn: "978-7-5321-0295-8", name: "围城", publish: "人民文学出版社", price: 32.0,
+    // inventory: 60, author: "钱钟书", keyword: "讽刺 小说"},
+    // {bookId: 7, isbn: "978-7-02-010804-5", name: "三体", publish: "重庆出版社", price: 68.0,
+    // inventory: 80, author: "刘慈欣", keyword: "科幻 小说"},
+    // {bookId: 8, isbn: "978-7-108-04348-1", name: "平凡的世界", publish: "人民文学出版社", price: 39.8,
+    // inventory: 70, author: "路遥", keyword: "现实主义 小说"},
+    // {bookId: 9, isbn: "978-7-123-45678", name: "新书", publish: "新出版社", price: 45.0,
+    // inventory: 252, author: "新作者", keyword: "新书 出版"}
 ])
 
 const searchText = ref('')
@@ -61,12 +61,12 @@ onMounted(() => {
     page.currentSubPage = '/bookManage'
     color.setOption(1)
 
-    //getBookList()
+    getBookList()
     isMounted.value = true
 })
 
 function getBookList(){
-    axios.get('', 
+    axios.get('/customer/book/order/0', 
     ).then(response => {
         books.value = response.data.data
     })
@@ -74,17 +74,19 @@ function getBookList(){
 }
 
 function getSearchBook(){
-    axios.get('',    
+    axios.get(`/customer/book/${searchText.value}`,
     ).then(response => {
         books.value = response.data.data
+        // books.value.
     })
     .catch(error => {alert(error)})
 }
 
-function deleteBook(){
-    axios.post('',    
+function deleteBook(bookId){
+    console.log(bookId)
+    axios.delete(`/business/deleteBook/${bookId}`,    
     ).then(response => {
-        books.value = response.data.data
+        alert(response.data.msg)
     })
     .catch(error => {alert(error)})
 }
