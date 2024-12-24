577e3be7ca5bdbb3e78576ef01f22c617c770830
TreeNode09 <StickpageX@hotmail.com>
2024-12-24 11:53:32 +0800

    添加了顾客购书界面

--- src/views/OutOfStock.vue
+++ src/views/OutOfStock.vue
@@ -6,10 +6,15 @@
 <main-button v-if="!isSelect" @click="startSelect">创建采购单</main-button>
 <plain-button v-if="isSelect" @click="endSelect">取消选择</plain-button>
 <main-button v-if="isSelect" @click="postSelect">确认选择</main-button>
+<el-select v-if="isSelect" v-model="selectedSupplier" placeholder="选择供书商..." class="search">
+    <el-option label="不指定" value="-1"/>
+    <el-option v-for="supplier in suppliers"
+        :label="supplier.supplierName" :value="supplier.supplierId" @change="changeSupplier"/>
+</el-select>
 <main-button @click="toAdd" class="right">添加缺书记录</main-button>
-<el-table v-if="isMounted" :data="stocks" ref="tableRef"
+<el-table v-if="isMounted" :data="filteredStocks" ref="tableRef"
     :row-class-name="setRowClass" @selection-change="handleSelectionChange">
-    <el-table-column v-if="isSelect" type="selection" width="50px"></el-table-column>
+    <el-table-column v-if="isSelect" type="selection" :selectable="isSelectable" width="50px"></el-table-column>
     <el-table-column v-else width="50px"></el-table-column>
     <el-table-column prop="stockId" label="缺货记录号"></el-table-column>
     <el-table-column prop="name" label="书名"></el-table-column>
@@ -47,9 +52,19 @@ const stocks = ref([
     {stockId: 1, bookId: 9, supplierId: "4", num: 81, date_: "2024-12-20 00:00:00", isbn: "978-7-123-45678",
     name: "新书", publish: "新出版社", price: 45.0, author: "新作者", supplierName: "新华书店", address: "北京市西城区西长安街丙17号"}
 ])
+const filteredStocks = ref([
+{stockId: 1, bookId: 9, supplierId: "4", num: 81, date_: "2024-12-20 00:00:00", isbn: "978-7-123-45678",
+name: "新书", publish: "新出版社", price: 45.0, author: "新作者", supplierName: "新华书店", address: "北京市西城区西长安街丙17号"}
+])
+
+const suppliers = ref([
+    {supplierId: 1, supplierName: '主图'},
+    {supplierId: 2, supplierName: '东图'}
+])
 
 const selectedRow = ref([])
 const selectedIndex = ref([])   //选中项的行号，不是id
+const selectedSupplier = ref(-1)
     
 onMounted(() => {
     page.currentUser = 'Admin'
@@ -65,7 +80,21 @@ function getStockList()
 {
     axios.get('business/stockList'
     ).then(response => {
-        stocks.value = response.data.data      
+        stocks.value = response.data.data
+        filteredStocks.value = stocks.value
+    })
+    .catch(error => {alert(error)})
+}
+
+function getStockListBySupplier()
+{
+    
+}
+
+function getSuppliers(){
+    axios.get(''
+    ).then(response => {
+        suppliers.value = response.data.data
     })
     .catch(error => {alert(error)})
 }
@@ -89,6 +118,10 @@ function setRowClass({row, rowIndex})
     return color
 }
 
+function isSelectable(){
+    return (selectedSupplier.value !== -1)
+}
+
 function startSelect()
 {
     isSelect.value = true
@@ -106,6 +139,11 @@ function handleSelectionChange(rows)
             }
         }
     }
+}
+
+function changeSupplier(value){
+    selectedSupplier.value = value
+    getStockListBySupplier()
 }
 
 function endSelect()
