761db177f7a60a4aa9ac6478bf3134662b2396ab
TreeNode09 <StickpageX@hotmail.com>
2024-12-26 10:41:44 +0800

    购物车显示原价和折扣价，余额不足时显示透支额度

--- src/stores/user.js
+++ src/stores/user.js
@@ -9,11 +9,20 @@ export const useUser = defineStore('user', () => {
         password:'password1234',
         userName:'张三',
         address:'北京市朝阳区望京街道',
-        balance:250,
+        balance:-250,
         creditLevel:4
     })
 
+    const userRight = ref([
+        {discount: 0, overdraw: 0},
+        {discount: 0.1, overdraw: 0},
+        {discount: 0.15, overdraw: 0},
+        {discount: 0.15, overdraw: 100},
+        {discount: 0.2, overdraw: 200},
+        {discount: 0.25, overdraw: '∞'},
+    ])
+
     const bookCart = ref([])
 
-    return {userInfo, bookCart}
+    return {userInfo, userRight, bookCart}
 })
